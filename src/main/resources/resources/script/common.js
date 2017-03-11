6336333bcedcae3115344f98309e2b313d7b22fc
renbaoyus <renbaoyus@163.com>
2017-03-11 10:47:43 +0800

    增加UI效果
    Change-Id: I4c3e821ec372d697ef42736440bd0e1edd5b0298
    Signed-off-by: renbaoyus <renbaoyus@163.com>

--- src/main/resources/resources/script/common.js
+++ src/main/resources/resources/script/common.js
@@ -1,5 +1,12 @@
+// 页面初始化
 $(function(){
+	// 绑定提交按钮动作
 	$("#submit").click(function(){
 		$("#submit").parents("form").submit();
+	});
+	
+	// 只读的checkbox依然可选择，做不可选择处理
+	$("input:checkbox[readonly]").click(function(){
+		return false;
 	});
 })
